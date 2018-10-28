7819975ad2c26b84c0a429818a1ca9f4ae5bccfb
Pawel <pawroz3@st.amu.edu.pl>
2018-10-28 12:42:05 +0100

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("enter an integer");
+        int myint = keyboard.nextInt();
+        System.out.println(myint);
+    }
+}
